ac3baed6c98c3ea4ea3b6b887d920604073117de
Deepak Kumar <deepakkumar302014@gmail.com>
2023-11-29 11:24:43 +0530

    access control methods updated

--- chaincode/abac.go
+++ chaincode/abac.go
@@ -5,34 +5,28 @@ import (
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
-// func isAdmin(caller cid.ClientIdentity) bool {
-// 	// Implement logic to check if the client identity is an admin
-// 	return true
-// }
-
-// func isFaculty(caller cid.ClientIdentity) bool {
-// 	// Implement logic to check if the client identity is a faculty
-// 	return true
-// }
-
 // isAdmin checks if the client identity has the "admin" role.
 func (s *StudentRecordContract) isAdmin(ctx contractapi.TransactionContextInterface, clientID cid.ClientIdentity) bool {
-	// Get the client identity
-	// clientID := ctx.GetClientIdentity()
 
 	// Get the attribute named "role" from the client's certificate
-	roleAttribute, found, _ := clientID.GetAttributeValue("role")
+	// roleAttribute, found, _ := clientID.GetAttributeValue("role")
 
 	// Check if the "role" attribute is present and set to "admin"
-	return found && roleAttribute == "admin"
+	// return found && roleAttribute == "admin"
+
+	//since role are not defined yet so return true in general
+	return true
 }
 
 func (s *StudentRecordContract) isFaculty(ctx contractapi.TransactionContextInterface, clientID cid.ClientIdentity) bool {
 	// Get the attribute named "role" from the client's certificate
-	roleAttribute, found, _ := clientID.GetAttributeValue("role")
+	// roleAttribute, found, _ := clientID.GetAttributeValue("role")
 
 	// Check if the "role" attribute is present and set to "faculty"
-	return found && roleAttribute == "faculty"
+	// return found && roleAttribute == "faculty"
+
+	//since role are not defined yet so return true in general
+	return true
 }
 
 // // isFacultyOfCourse checks if the client identity matches the faculty ID of the given course.
@@ -43,3 +37,4 @@ func (s *StudentRecordContract) isFaculty(ctx contractapi.TransactionContextInte
 // 	// Check if the "facultyID" attribute is present and matches the course's faculty ID
 // 	return found && facultyIDAttribute == courseMapping[courseID].FacultyID
 // }
+
